2edf56f7c6be1c331aa9e19dae7dc768532ec5d3
Krta182 <kristijan.krtalic002@gmail.com>
2020-04-27 17:38:09 +0200

    Push to my mrezno prog rep

--- MreznoProgramiranje-vjezba6_KristijanKrtalic/PortSkenerVj6.py
+++ MreznoProgramiranje-vjezba6_KristijanKrtalic/PortSkenerVj6.py
@@ -1,97 +1,70 @@
 import datetime
 import socket
+import sys
 from queue import Queue
 import threading
 
 
 def scanPort(port):
-    sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    target =input(">>")
-    targetIP = socket.gethostbyname(target)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # sock = socket.socket()
+    print_lock = threading.Lock()
     with print_lock:
-        print ("Skeniram host %s, IP adresu: %s" % (target, targetIP))
-        print("Skeniram port: %s" %(port)
-        # sock = socket.socket()
+        print("Skeniram host {}, IP adresu: {}".format(target, targetIP))
+        print("Skeniram port: {}".format(port))
+        # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((targetIP, port))
         if result == 0:
-            print ("Port %s otvoren" %(port))
+            print("Port {} otvoren".format(port))
         else:
-            print ("port je zatvoren %s" %(port))
-                sock.close()
-    
-    except KeyboardInterrupt:
-        print ("Pritisili ste Ctrl+C")
-        sys.exit()
+            print("port {} je zatvoren ".format(port))
+        sock.close()
 
-    except socket.gaierror:
-        print ("Hostname je nepoznat.Izlazim iz programa...")
-        sys.exit()
 
-    except socket.error:
-        print ("Nemogu se spojiti na server")
-        sys.exit()    
-        
-def worker():
+def threader():
     while True:
         worker = q.get()
         scanPort(worker)
-        q.task_done()    
-        
-def execute():        
+        q.task_done()
 
-    print ("Unesite od kojeg do kojeg porta zelite napraviti skeniranje?")
-    start =input("Unesite Pocetni port >> ")
-    end =input(" Unesite Zavrsni port >> ")
 
-      
-    print ("Unesite broj niti:")
-    num_worker_threads=int(input(">>"))
+def execute():
+    global q, target, targetIP
+
+    print("Vrijeme pokretanja ovog programa:")
+    t1 = datetime.datetime.now()
+    print(t1)
+    print("----------------------------------------------")
+
+    print("Molim vas unesite adresu hosta koju zelite testirati:")
+    target = input(">> ")
+    targetIP = socket.gethostbyname(target)
+
+    print("Unesite od kojeg do kojeg porta zelite napraviti skeniranje?")
+    start = input("Unesite Pocetni port >> ")
+    end = input("Unesite Zavrsni port >> ")
+
+    print("Unesite broj niti:")
+    num_worker_threads = int(input(">> "))
 
     q = Queue()
-    threads = []
-    
-    for i in range(num_worker_threads):
-            t = threading.Thread(target=worker)
-            t.start()
-            threads.append(t)
-
-
-    for worker in range(int(start),int(end)):
-            q.put(worker)
-            q.join()
-
 
     for i in range(num_worker_threads):
-            q.put(None)
-        for t in threads:
-            t.join()
-        
+        t = threading.Thread(target = threader)
+        t.daemon =True
+        t.start()
 
+    for worker in range(int(start), int(end) + 1):
+        q.put(worker)
+
+    q.join()
 
     t2 = datetime.datetime.now()
+    total = t2 - t1
+    print("Skeniranje gotovo u: %s" % (total))
 
-    total =  t2 - t1
+# Start Programa
+if __name__ == '__main__':
 
-    print ("Skeniranje gotovo u: %s" %(total))
-
-
-
-
-
-
-
-#Start Programa
-
-print ("Vrijeme pokretanja ovog programa:")
-t1=datetime.datetime.now()
-print (t1)
-
-print ("----------------------------------------")
-print ("Molim vas unesite adresu hosta koju zelite testirati:")
-
-execute()
-
-
-
-
-
+    print("----------------------------------------")
+    execute()
