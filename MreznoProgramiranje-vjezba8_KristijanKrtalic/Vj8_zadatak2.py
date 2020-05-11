e5f6ffadc2dc01f942028e8633c7631220e0771a
Krta182 <kristijan.krtalic002@gmail.com>
2020-05-11 19:51:56 +0200

    push to update my mrezno prog rep vj 8

--- MreznoProgramiranje-vjezba8_KristijanKrtalic/Vj8_zadatak2.py
+++ MreznoProgramiranje-vjezba8_KristijanKrtalic/Vj8_zadatak2.py
@@ -1,2 +1,10 @@
+import smtplib
 
+client = smtplib.SMTP('localhost',1025)
 
+fromaddr = 'kristijan.krtalic002gmail.com'
+toaddrs = 'kristijan.krtalic002@gmail.com'
+msg = 'Hello'
+
+client.sendmail(fromaddr, toaddrs, msg)
+client.quit()
